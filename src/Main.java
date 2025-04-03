36a33e7b044eddd2cbc4d615b4558880d63ab7d0
MiguelM1004 <munozlopez3000@gmail.com>
2025-04-02 21:06:04 -0500

    Se hace captura de valores y se calcula el salario neto

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,7 @@ public class Main {
         double valorHora = 0;
         double cantidadHoras = 0;
         double bonificacionEmpleado = 0;
+        double valorSalarioMinimo = 0;
         int auxilioTransporte = 200000;
 
         System.out.println("Ingrese su usuario: ");
@@ -39,6 +40,27 @@ public class Main {
         contrasenaUsuario = sc.nextLine();
         if (usuarioEmpleado.equals("admin") && contrasenaUsuario.equals("admin")){
             System.out.println("Bienvenido al sistema");
+            System.out.println("Ingrese el nombre del empleado: ");
+            nombreEmpleado = sc.nextLine();
+            System.out.println("Ingrese su documento de identidad: ");
+            documentoIdentidad = sc.nextLine();
+            System.out.println("Ingrese el día de descanso (Lunes a Viernes): ");
+            diaDescanso = sc.nextLine();
+            System.out.println("Ingrese el valor de la hora: ");
+            valorHora = sc.nextDouble();
+            System.out.println("Ingrese la cantidad de horas trabajadas: ");
+            cantidadHoras = sc.nextDouble();
+            salarioBruto = valorHora * cantidadHoras;
+            System.out.println("Ingrese el valor del salario minimo para este año: ");
+            valorSalarioMinimo = sc.nextDouble();
+            if (salarioBruto > (valorSalarioMinimo*2)){
+                bonificacionEmpleado = 0;
+            } else {
+                bonificacionEmpleado = salarioBruto*0.1;
+                auxilioTransporte = 200000;
+            }
+            salarioNeto = salarioBruto + bonificacionEmpleado + auxilioTransporte;
+            System.out.println("El salario neto es: " + salarioNeto);
         } else {
             System.out.println("Error de credenciales ");
         }
